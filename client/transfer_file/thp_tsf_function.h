dcd4e6a3b83e0b59e309607890df1912ea9a0062
dsw <1138222970@qq.com>
2025-03-04 09:21:51 +0800

    补漏提交

--- client/transfer_file/thp_tsf_function.h
+++ client/transfer_file/thp_tsf_function.h
@@ -1,6 +1,10 @@
 #ifndef __THP_TSF_FUNCTION_H
 #define __THP_TSF_FUNCTION_H
-
+#include "../frame/tast_queue.h"
+#include "../network/tcp_connect.h"
+#include "../frame/cmd_tast.h"
+#include "../config/config.h"
 void* thp_tsf_function(void* arg);
 
 #endif
+
