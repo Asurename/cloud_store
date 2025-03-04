64d5ad19ffe5c17008c0dff8feeb822db0afe47c
dsw <1138222970@qq.com>
2025-03-04 14:06:58 +0800

    完成传输模块v1.0

--- server/threadpoll_function/thp_tsf_function.c
+++ server/threadpoll_function/thp_tsf_function.c
@@ -1,42 +1,54 @@
 //服务端
 #include "../lib.h"
 
-void handl_upload(cmd_tast* t, int socketfd){
-/*
-    //3 接受并解析结构体
-        //提取出文件名
-        //提取出当前路径
-        //提取出虚拟绝对文件路径
-        //提取这个文件哈希值
-        //提取文件大小
+void handl_upload(int socketfd){
+    //接受上传文件
+    printf("正在接受来自客户端xxx的文件...\n");//可加客户ip
 
-    //4 遍历hash值表
-    if(哈希值不存在){//即文件内容不存在，需要接收文件内容，存储路径是pwd/fileshouse/哈希值
-        //5 发送哈希值不存在的标志。
-            //7 接受文件内容
-        //8 在数据库 表插入一行
-            //插入函数
-        //执行到这里，就说明文件已经接收完毕，并且已经存储到服务器的fileshouse文件夹中和数据库中，结束线程
+    FILE *fp;
+    char buffer[4096] = {0};
+    fp = fopen("../fileshouse_server/500mb_file", "wb");
+    if (fp == NULL) {
+        perror("File open failed");
     }
-    
-    if(哈希值存在){//即文件内容已存在，不需要接收文件内容，只需要在数据库 表插入一行。秒传！
-        //5 发送哈希值存在的标志。
-        if(虚拟绝对路径存在){
-            //什么都不做，秒传，结束线程
-        }else{
-            //表插入一行
+    int bytes_received;
+    while ((bytes_received = recv(socketfd, buffer, 4096, 0)) > 0) {
+        fwrite(buffer, 1, bytes_received, fp);
+    }
+    printf("File received and saved as 'received_file.txt'\n");
+    close(socketfd);
+    printf("客户端xxx上传文件完成，断开数据TCP连接");
+}
+
+void handl_download(int socketfd){
+         //发送下载文件
+        printf("下载处理中...\n");
+        
+        char buffer[4096] = {0};
+        FILE *fp;
+        fp = fopen("../fileshouse_server/1000mb_file", "rb");
+        if (fp == NULL) {
+            perror("File open failed");
         }
-    }
+        // 获取文件大小
+        fseek(fp, 0, SEEK_END);
+        long file_size = ftell(fp);
+        fseek(fp, 0, SEEK_SET);
 
-*/  
+        // 发送文件大小给客户端
+        send(socketfd, &file_size, sizeof(file_size), 0);
 
+        // 发送文件内容
+        int bytes_read;
+        while ((bytes_read = fread(buffer, 1, 4096, fp)) > 0) {
+            send(socketfd, buffer, bytes_read, 0);
+        }
+
+        printf("File sent successfully.\n");
+        close(socketfd);    
+        printf("客户端xx下载任务完成,断开数据TCP连接\n");
 
 }
-
-void handl_download(cmd_tast* t, int socketfd){
-    
-}
-
 
 
 //文件传输线程要执行的函数
@@ -54,20 +66,12 @@ void* thp_tsf_function(void * arg){
     printf("已和客户端子线程函数建立了TCP连接\n");
     //----------------------已和客户端子线程函数建立了TCP连接------------------------------
     if(t->cmdType == CMD_TYPE_UPLOAD){
-        //上传文件
-        handl_upload(t, socketfd);
-        printf("上传/下载任务处理中...\n");
-        sleep(1);
+        handl_upload(socketfd);
     }
     if(t->cmdType == CMD_TYPE_DOWNLOAD){
-        //下载文件
-        handl_download(t, socketfd);
-        printf("上传/下载任务处理中...\n");
-        sleep(1);
+        handl_download(socketfd);
     }
 
-    close(socketfd);
-    printf("任务完成,断开TCP数据连接\n");
     return NULL;
 }
 
