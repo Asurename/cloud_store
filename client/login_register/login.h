af6e1800bd74ebeef0547822bcf8e2d843012f3d
王梓航 <1272172906@qq.com>
2025-03-04 22:13:29 +0800

    补全了login.h里相关函数的声明

--- client/login_register/login.h
+++ client/login_register/login.h
@@ -6,4 +6,10 @@ void login_01(cmd_tast* t,int* connect_fd);
 void login_02(cmd_tast* t,int* connect_fd);
 void crypt_password(cmd_tast* t,char buf[],int len);
 
+//读取用户的输入相关
+int input_client_info(const char * logintips,char *buffer,int length);
+
+//纠错
+void error(int status, int err, const char *fmt,...);
+
 #endif
