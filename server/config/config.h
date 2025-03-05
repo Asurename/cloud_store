0540f5f2997e9cbe6408aba275a843d8210738d8
suis <634049075@qq.com>
2025-03-05 09:57:15 +0800

    add some new color

--- server/config/config.h
+++ server/config/config.h
@@ -56,6 +56,14 @@ typedef struct {
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW   "\x1b[33m"
+#define ANSI_COLOR_BULE   "\x1b[34m"
+#define ANSI_COLOR_HIGHLIGHT   "\x1b[1m"
+#define ANSI_COLOR_START   "\x1b[5m"
+#define ANSI_COLOR_NOSTART   "\x1b[25m"
+#define ANSI_COLOR_PURPLE   "\x1b[35m"
+
 
  char *read_file(const char *filename);
  int parse_config(const char *filename, Config *config);
